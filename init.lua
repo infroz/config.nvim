6f7a5b87a34334a8b4381dbb76dbb51d0cdf882c
infroz <infroz98@gmail.com>
2025-01-02 12:07:04 +0100

    added lazy

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
+require("config.lazy")
 
+vim.wo.relativenumber = true
+vim.wo.number = true
+
+-- Highlight yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (Copying text)',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
