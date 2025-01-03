ebe6081943ad8e3ea958bf483a25e6d5e3910878
infroz <infroz98@gmail.com>
2025-01-03 14:44:01 +0100

    added simple buffer management

--- init.lua
+++ init.lua
@@ -8,6 +8,11 @@ vim.opt.tabstop = 2
 vim.opt.expandtab = false
 vim.opt.clipboard = 'unnamedplus' -- paste and yank uses system clipboard
 
+-- buffer management
+vim.keymap.set('n', '<leader>bo', '<cmd>%bd|e#!<CR>', { desc = 'Delete other buffers' })
+vim.keymap.set('n', '<leader>bd', '<cmd>bd!<CR>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', '<leader>ba', '<cmd>1,$bd!<CR>', { desc = 'Delete * buffers' })
+
 -- Highlight yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (Copying text)',
