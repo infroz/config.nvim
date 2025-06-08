cc242c9bca511b7f116e0194d28e4ae17c514f6f
infroz <infroz98@gmail.com>
2025-06-08 10:50:18 +0200

    fixed gdscript

---
+++ after/ftplugin/gdscript.lua
@@ -0,0 +1,22 @@
+-- Get the port from environment variable and convert to number
+local port = tonumber(os.getenv('GDScript_Port') or '6005')
+if not port then
+	vim.notify('Invalid GDScript_Port: must be a number', vim.log.levels.ERROR)
+	return
+end
+
+-- Connect to the language server
+local cmd = vim.lsp.rpc.connect('127.0.0.1', port)
+
+-- Pipe used for Godot to open files in nvim
+local pipe = '/tmp/godot.pipe'
+
+vim.lsp.start({
+	name = 'Godot',
+	cmd = cmd,
+	root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+	on_attach = function(client, bufnr)
+		-- Start the server to listen for file open requests from Godot
+		vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+	end,
+})
